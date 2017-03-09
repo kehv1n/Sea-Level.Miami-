5df050ca142bd80ac2128cfffc48a3a986ec5d19
Kevin D. Silva <kevin.dasilva1@gmail.com>
2017-03-09 11:01:42 -0500

    currently working on parsing JSOn data to find specific details of the tides

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ const User = require('../models/users.js');
 const passport = require('passport');
 
 // Bcrypt to encrypt passwords
-const bcrypt     = require("bcrypt");
+const bcrypt = require("bcrypt");
 const bcryptSalt = 10;
 
 
@@ -60,102 +60,150 @@ indexRoute.post('/dashboard', (req, res, next) => {
                 next(err);
                 return;
             }
-            const day = new Date ();
+            const day = new Date();
             let dd = day.getDate();
-            let mm = day.getMonth() + 1 ; //Starts at 0
+            let mm = day.getMonth() + 1; //Starts at 0
             const yy = day.getFullYear();
 
-            if(mm < 10) {
-              mm = '0'+mm;
+            if (mm < 10) {
+                mm = '0' + mm;
             }
-            if(dd < 10) {
-              dd = '0'+dd;
+            if (dd < 10) {
+                dd = '0' + dd;
             }
 
-             //DATE USER MAKES POST REQ
+            //DATE USER MAKES POST REQ
             const today = `${yy}${mm}${dd}`;
 
             const link = `https://tidesandcurrents.noaa.gov/api/datagetter?station=${buoys.stationID}&product=water_level&time_zone=gmt&format=json&begin_date=${today}&end_date=${today}&units=metric&datum=msl`;
             ///Returns live data from Buoy////
             request(link, function(error, response, body) {
-              if (error) {
-                next(error);
-                return;
-              }
+                if (error) {
+                    next(error);
+                    return;
+                }
+                const parsedTimes = [];
+                const parsedLevels = [];
+                //Parse the JSON FILES WITH THE data
+                //We want to have
+               JSON.parse(body, ( name, value ) => {
+                    if (name === 't' ) {
+                      parsedTimes.push(value);
+                      return;
 
-              res.render('dashboard', {
-                zipcode: userZip,
-                coords: userCoords,
-                buoyID: buoys.stationID,
-                buoyName: buoys.buoyName,
-                buoyData: body
+                    }
+                    if (name === 'v'){
+                      parsedLevels.push(value);
+                      return;
+                    }
+                    else {
+                      return undefined;
+                    }
 
-            });
+                  });
+                  //Turn the string of "dates" to actual dates
+                  parsedTimes.forEach((time) =>{
+                    let eachTime = new Date(time);
+                    let eachTimeRangeMAX = new Date(eachTime);
+                    let eachTimeRangeMIN = new Date(eachTime);
+                    //Find the closest time with Range (+6/-6)
+                    eachTimeRangeMAX.setMinutes(eachTime.getMinutes() + 6);
+                    eachTimeRangeMIN.setMinutes(eachTime.getMinutes() - 6);
+
+                  //  eachTime.setMinutes(eachTime.getMinutes() + 6);
+                   //
+                  //   eachTime.setMinutes(eachTime.getMinutes() - 6);
+
+                    if (eachTimeRangeMAX >= day && eachTimeRangeMIN <= day) {
+                      const currentTimeIndex = parsedTimes.indexOf(eachTime);
+                      console.log(currentTimeIndex);
+                    }
+                  });
+                  res.redirect('/');
+                  //
+                  // console.log(parsedData);
+                //
+                // res.render('dashboard', {
+                //     zipcode: userZip,
+                //     coords: userCoords,
+                //     buoyID: buoys.stationID,
+                //     buoyName: buoys.buoyName,
+                //     buoyData: body
+
+                });
 
             });
 
         });
-    });
+    // });
 });
 
 /* GET LOGIN */
 indexRoute.get('/login', (req, res, next) => {
-  res.render('login.ejs',{
-    errorMessage: req.flash('error')//req.flash returns array
-  });
+    res.render('login.ejs', {
+        errorMessage: req.flash('error') //req.flash returns array
+    });
 });
 
 // POST LOGIN
 indexRoute.post("/login",
-  passport.authenticate("local", {
-  successReturnToOrRedirect: "/",
-  failureRedirect: "/login",
-  failureFlash: true,
-  successFlash: 'You successfully logged in',
-  passReqToCallback: true
-}));
+    passport.authenticate("local", {
+        successReturnToOrRedirect: "/",
+        failureRedirect: "/login",
+        failureFlash: true,
+        successFlash: 'You successfully logged in',
+        passReqToCallback: true
+    }));
 
 /* GET REGISTER */
 indexRoute.get('/register', (req, res, next) => {
-  res.render('register');
+    res.render('register');
 });
 
 /* POST REGISTER */
 indexRoute.post('/register', (req, res, next) => {
-  const username = req.body.username;
-  const password = req.body.password;
-  res.render('register');
+    const username = req.body.username;
+    const password = req.body.password;
+    res.render('register');
 
 
-  if (username === "" || password === "") {
-      res.render('/register', { message: "Please make sure you fill both fields" });
-      return;
+    if (username === "" || password === "") {
+        res.render('/register', {
+            message: "Please make sure you fill both fields"
+        });
+        return;
     }
 
-    User.findOne({ username }, "username", (err, user) => {
-    if (user !== null) {
-      res.render('/register', { message: "You're already part of the club!" });
-      return;
-    }
+    User.findOne({
+        username
+    }, "username", (err, user) => {
+        if (user !== null) {
+            res.render('/register', {
+                message: "You're already part of the club!"
+            });
+            return;
+        }
 
-  const salt     = bcrypt.genSaltSync(bcryptSalt);
-  const hashPass = bcrypt.hashSync(password, salt);
+        const salt = bcrypt.genSaltSync(bcryptSalt);
+        const hashPass = bcrypt.hashSync(password, salt);
 
-  const newUser =  User({
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    userName: username,
-    encryptedPassword: hashPass
-  });
+        const newUser = User({
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            userName: username,
+            encryptedPassword: hashPass
+        });
 
-  newUser.save((err) => {
-      if (err) { // IF problem
-        res.render("/register", { message: "Something went wrong" });
-      } else { // If no problem ;)
-        req.flash('success' , 'You have been registered. Welcome to the wave.');
-        res.redirect("index");
-      }
-      });
+        newUser.save((err) => {
+            if (err) { // IF problem
+                res.render("/register", {
+                    message: "Something went wrong"
+                });
+            } else { // If no problem ;)
+                req.flash('success', 'You have been registered. Welcome to the wave.');
+                res.redirect("index");
+            }
+        });
     });
 });
 
